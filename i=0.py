80427f982e68dd380040a08a91c13cd9bf581000
ELIAS24RIVERA <1515885@senati.pe>
2024-08-21 12:58:09 -0500

    suma

--- i=0.py
+++ i=0.py
@@ -1,1 +1,5 @@
-print("hola mundo")
+Numeros=[1,2,3,4]
+Datos=[4,"Una cadena",-5,1.5,"Otra cadena"]
+
+print(Numeros)
+print(Datos)
---
+++ suma.py
@@ -0,0 +1,3 @@
+n1 = int(input("ingrese el mrimer numero: "))
+n2 = int(input("ingrese el segundo numero: "))
+print "la suma de dos numeros es: " + str(n1 + n2)
